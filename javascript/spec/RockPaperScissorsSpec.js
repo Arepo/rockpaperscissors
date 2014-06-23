5b6133b6657eeca237f3447570b0135a4d0111a3
Sasha Cooper <jinksy@gmail.com>
2014-06-23 16:27:02 +0100

    introduced tests to *cough* drive RPSLSp

--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -18,10 +18,24 @@ describe("Rock-Paper-Scissors", function() {
         expect(game.winner()).toBe(player1);
       });
 
+      it('should beat lizard', function() {
+        player1.picks('rock');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1);
+      });
+
       it('should lose to paper', function() {
 
         player1.picks('rock');
         player2.picks('paper');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+        it('should lose to Spock', function() {
+
+        player1.picks('rock');
+        player2.picks('Spock');
         expect(game.winner()).toBe(player2);
 
       });
@@ -38,10 +52,26 @@ describe("Rock-Paper-Scissors", function() {
 
       });
 
+      it('should beat Spock', function() {
+
+        player1.picks('paper');
+        player2.picks('Spock');
+        expect(game.winner()).toBe(player1);
+
+      });
+
       it('should lose to scissors', function() {
 
         player1.picks('paper');
         player2.picks('scissors');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+      it('should lose to lizard', function() {
+
+        player1.picks('paper');
+        player2.picks('lizard');
         expect(game.winner()).toBe(player2);
 
       });
@@ -58,10 +88,26 @@ describe("Rock-Paper-Scissors", function() {
 
       });
 
+      it('should beat lizard', function() {
+
+        player1.picks('scissors');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1);
+
+      });
+
       it('should lose to rock', function() {
 
         player1.picks('scissors');
         player2.picks('rock');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+      it('should lose to Spock', function() {
+
+        player1.picks('scissors');
+        player2.picks('Spock');
         expect(game.winner()).toBe(player2);
 
       });
