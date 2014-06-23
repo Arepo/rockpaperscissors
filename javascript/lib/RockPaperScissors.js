39420d17e4c0470b9e8a3b017610196aed6a3526
Sasha Cooper <jinksy@gmail.com>
2014-06-23 13:43:28 +0100

    ugly but working js code

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -6,13 +6,63 @@ Player.prototype.picks = function(pick) {
   this.pick = pick;
 };
 
+Player.prototype.pick = function() {
+  this.pick
+}
+
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
 };
 
 Game.prototype.winner = function() {
-  if(1 === 1) {
+
+  if(this.player1.pick === "rock" && this.player2.pick === "paper") {
+    return this.player2;
+  
+  } else if(this.player1.pick === "rock" && this.player2.pick === "scissors") {
     return this.player1;
-  }
+  
+  } else if(this.player1.pick === "scissors" && this.player2.pick === "paper") {
+    return this.player1;
+  
+  } else if(this.player1.pick === "scissors" && this.player2.pick === "rock") {
+    return this.player2;
+  
+  } else if(this.player1.pick === "paper" && this.player2.pick === "scissors") {
+    return this.player2;
+  
+  } else if(this.player1.pick === "paper" && this.player2.pick === "rock") {
+    return this.player1;
+  
+  } else {
+  	return null
+  };
 };
+
+// class Player
+// 	def initialize(name) 
+// 		@name = name
+//  	end
+
+//  	def picks(pick)
+//  		@pick = pick
+//  	end
+
+//  	def pick
+//  		@pick
+//  	end
+// end
+
+// class Game
+// 	def initialize(player1, player2)
+// 		@player1 = player1
+// 		@player2 = player2
+// 	end
+
+// 	def winner
+// 		return @player1 if 1 == 1
+// 	end
+
+// end
+
